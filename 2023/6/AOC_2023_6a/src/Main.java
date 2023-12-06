e4ad8367f47c97f2d9a0af864e9f7dece5e4403f
Matyas.Horky <horkyskolni@gmail.com>
2023-12-06 10:22:17 +0100

    6
    6

---
+++ 2023/6/AOC_2023_6a/src/Main.java
@@ -0,0 +1,50 @@
+import java.awt.*;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.lang.Math.ceil;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        testCalc();
+        int score = 1;
+        List<String> lines =  Files.readAllLines(Paths.get("C:\\Users\\Horky\\IdeaProjects\\AOC_2023_6\\src\\input.txt"), StandardCharsets.UTF_8);
+
+        String[] times = lines.get(0).substring(5).trim().replaceAll(" +", " ").split(" ");
+        String[] distances = lines.get(1).substring(9).trim().replaceAll(" +", " ").split(" ");
+        for(int i = 0; i < times.length; i++){
+            score *= calcRaces(times[i],distances[i]);
+        }
+        System.out.println(score);
+    }
+
+    private static void testCalc() {
+        System.out.println("test: " + String.valueOf(calcRaces("5","8")));
+    }
+
+    private static int calcRaces(String time, String distance) {
+        double hold = ceil(Float.valueOf(time)/2);
+        double run = Float.valueOf(time)-hold;
+        double limit = Float.valueOf(distance);
+//        System.out.println("hold: "+String.valueOf(hold));
+//        System.out.println("run: " + String.valueOf(run));
+        int score = 0;
+        while(true){
+            if(hold * run > limit){
+                score++;
+                hold = hold - 1f;
+                run = run + 1f;
+//                System.out.println("hold: "+String.valueOf(hold));
+//                System.out.println("run: " + String.valueOf(run));
+            }else break;
+        }
+        if(score == 0) return 0;
+        if(Integer.valueOf(time)%2 == 0) return 2*score-1;
+        else return 2*score;
+    }
+}
